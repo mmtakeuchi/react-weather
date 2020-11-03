c04c3413b4d963f3a715ef7be3b19f58e575d8c3
mmtakeuchi <mikemtaki@gmail.com>
2020-11-03 11:43:24 -0700

    uppercase description

--- weather/src/components/WeatherCard.js
+++ weather/src/components/WeatherCard.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 const WeatherCard = ({city, description, temperature, mainWeather, minTemp, maxTemp, error}) => {
+    const capitalize = (words) => {
+        return words.split(" ").map(word => word[0].toUpperCase() + word.slice(1)).join(" ")
+    }
+
     return (
         <div>
             {city && <p>{city}</p>}
-            {description && <p>{description}</p>}
+            {description && <p>{capitalize(description)}</p>}
             {temperature && <p>{temperature} °F</p>}
             {maxTemp && <p>H:{maxTemp}°F</p>}
             {minTemp && <p>L:{minTemp}°F</p>}
