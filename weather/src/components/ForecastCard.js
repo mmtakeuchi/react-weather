c659d1ed5ee59e54267b104c31c6a4f67146b637
mmtakeuchi <mikemtaki@gmail.com>
2021-02-12 12:42:38 -0700

    added media query for forecast container

--- weather/src/components/ForecastCard.js
+++ weather/src/components/ForecastCard.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React from "react";
 
-const ForecastCard = ({data, degree}) => {
+const ForecastCard = ({ data, degree }) => {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const d = new Date(data.dt * 1000);
+  const dayName = days[d.getDay()];
 
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const d = new Date(data.dt * 1000);
-    const dayName = days[d.getDay()];
+  const fahrenheit = (temp) => Math.round(temp);
+  const celsius = (temp) => Math.round(((temp - 32) * 5) / 9);
 
-    const fahrenheit = (temp) => Math.round(temp);
-    const celsius = (temp) => Math.round((temp - 32) * 5/9);
+  return (
+    <div className="forecast-card">
+      <h3 className="forecast-day">{dayName}</h3>
+      <p className="forecast-temp">
+        <span>
+          {degree === false
+            ? fahrenheit(data.main.temp_max.toFixed(0)) + "° / "
+            : celsius(data.main.temp_max.toFixed(0)) + "° / "}
+        </span>
+        <span>
+          {degree === false
+            ? fahrenheit(data.main.temp_min.toFixed(0)) + "°"
+            : celsius(data.main.temp_min.toFixed(0)) + "°"}
+        </span>
+      </p>
+      <img
+        className="forecast-icon"
+        src={`https://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+        alt="weather icon"
+      />
+      <p className="forecast-text">{data.weather[0].description}</p>
+    </div>
+  );
+};
 
-    return (
-        <div className="forecast-card">
-            <h3 className="forecast-day">{dayName}</h3>
-            <p className="forecast-temp">
-                <span>{degree === false ? fahrenheit(data.main.temp_max.toFixed(0)) + "° / " : celsius(data.main.temp_max.toFixed(0)) + "° / "}</span>
-                <span>{degree === false ? fahrenheit(data.main.temp_min.toFixed(0)) + "°" : celsius(data.main.temp_min.toFixed(0)) + "°"}</span> 
-            </p>
-            <img className="forecast-icon" src={`https://openweathermap.org/img/w/${data.weather[0].icon}.png`} alt="weather icon"/>
-            <p className="forecast-text">{data.weather[0].description}</p>
-    
-        </div>
-    )
-}
-
-export default ForecastCard
+export default ForecastCard;
